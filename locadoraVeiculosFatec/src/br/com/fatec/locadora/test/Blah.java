20ce1e832a5783d3ecd32de6b2f99da3fd498302
hrichardvieira <hrichardvieira@outlook.com>
2015-08-17 18:29:20 -0300

    Alteração no projeto

--- locadoraVeiculosFatec/src/br/com/fatec/locadora/test/Blah.java
+++ locadoraVeiculosFatec/src/br/com/fatec/locadora/test/Blah.java
@@ -1,14 +1,40 @@
 package br.com.fatec.locadora.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import org.junit.Test;
 
 import br.com.fatec.locadora.core.LiquibaseRunnerService;
 
 public class Blah {
 
-	@Test
+/*	@Test
 	public void test() {
 		LiquibaseRunnerService.run();
+	}*/
+	
+	@Test
+	public void testeConnection(){
+		LiquibaseRunnerService.run();
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:hsqldb:mem:fatec", "SA", "");
+
+			PreparedStatement stmt = conn.prepareStatement("insert into LOC_ACESSORIO values (?,?);");
+			
+			stmt.setLong(1, 10);
+			stmt.setString(2, "Roda de liga leve");
+			
+			stmt.getResultSet();
+			stmt.execute();
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Erro na execução do Liquibase", e);
+		}
 	}
 
 }
